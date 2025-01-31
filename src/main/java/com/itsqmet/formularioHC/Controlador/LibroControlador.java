3b6fecf3add9ca5a5b3457a21365b2652e768751
CrasherG4 <cgp4762@gmail.com>
2025-01-31 17:11:28 -0500

    Avance 2

--- src/main/java/com/itsqmet/formularioHC/Controlador/LibroControlador.java
+++ src/main/java/com/itsqmet/formularioHC/Controlador/LibroControlador.java
@@ -1,4 +1,36 @@
 package com.itsqmet.formularioHC.Controlador;
 
+import com.itsqmet.formularioHC.Entidad.Libro;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.time.LocalDate;
+
+@Controller
 public class LibroControlador {
+
+    @GetMapping("/formulario")
+    public String mostrarFormulario(Model model){
+        Libro libro = new Libro();
+        model.addAttribute("libro", libro);
+        return "pages/formulario";
+    }
+
+    @PostMapping("/registrar")
+    public String registrarLibro(@Valid @ModelAttribute Libro libro, BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+            model.addAttribute("errores", bindingResult.getAllErrors());
+            return "pages/formulario";
+        }else{
+            LocalDate fechaActual = LocalDate.now();
+            model.addAttribute("fechaActual", fechaActual);
+
+            return "pages/registroExitoso";
+        }
+    }
 }
--- src/main/java/com/itsqmet/formularioHC/Entidad/Libro.java
+++ src/main/java/com/itsqmet/formularioHC/Entidad/Libro.java
@@ -1,4 +1,84 @@
 package com.itsqmet.formularioHC.Entidad;
 
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class Libro {
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String nombre;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String autor;
+
+    @NotBlank
+    @Email(message = "Ingrese el correo en formato válido")
+    private String email;
+
+    @Size(min = 10, max = 10)
+    @Pattern(regexp = "\\d{10}")
+    private String telefono;
+
+    @Size(min = 10, max = 150)
+    private String domicilio;
+
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    private Date fechaEntrega;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public Date getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(Date fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
 }
